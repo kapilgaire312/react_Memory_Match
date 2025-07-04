53306899b84f53eb3946e62ac060970198b74bdd
Kapil Gaire <Kap@KapilMacBook-Pro.local>
2025-07-04 09:54:03 +0545

    -append

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ function Header({ addCards, setTurns }) {
   return (
     <div className="text-center">
       <h1 className="font-bold text-3xl my-4">Memory Match</h1>
-      <button className="border-2 border-black p-1 bg-purple-200" onClick={() => { addCards(), setTurns(0) }}>New Match</button>
+      <button className="border-2 border-black p-1 bg-purple-200 hover:opacity-80 active:opacity-60" onClick={() => { addCards(), setTurns(0) }}>New Match</button>
     </div>
   )
 
